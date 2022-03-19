c7f6e249a8c8045212973b98776a3bdf72bcd5a6
Kenny <mccormick84@gmail.com>
2022-03-19 14:17:40 +0900

    구글 맵 사용 설정

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:attendance_check/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
